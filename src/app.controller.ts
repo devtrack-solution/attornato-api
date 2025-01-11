a8bcdb8003058a98cf098c3eb45fe0c4b6a9eae6
Wilton Ferreira <wilto@devtrack.com.br>
2025-01-11 17:48:05 -0300

    fix: adjust e2e tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, HttpException } from "@nestjs/common";
 import { AppService } from './app.service'
+import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Throttle({default: { limit: 10, ttl: 60 }})
   @Get()
   getHello(): string {
-    console.log('This endpoint is protected by throttling')
-    return this.appService.getHello()
+      console.log('This endpoint is protected by throttling')
+      return this.appService.getHello()
   }
 }
