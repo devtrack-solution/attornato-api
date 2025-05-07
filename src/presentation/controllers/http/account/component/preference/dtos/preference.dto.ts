957565c8734c09de5bb282bfe44de572dcb3589e
Laércio Ávila <laercio.aaavila@gmail.com>
2025-05-07 18:10:47 -0300

    fix

--- src/presentation/controllers/http/account/component/preference/dtos/preference.dto.ts
+++ src/presentation/controllers/http/account/component/preference/dtos/preference.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger'
 import { BasicDto } from '@/presentation/controllers/http/dtos/basic.dto'
 
 export class PreferenceDto extends BasicDto {
-  @ApiProperty({ example: '#ff0000', required: true })
+  @ApiProperty({ required: true })
   accountId!: string
 
   @ApiProperty({ example: 'MAIN_COLOR', required: true })
