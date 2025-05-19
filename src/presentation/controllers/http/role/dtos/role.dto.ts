b4e86fcc64bcc0df8922e1e7b90364f52a2d447d
Laercio <laercio.aaavila@gmail.com>
2025-05-19 10:17:48 -0300

    improvement

--- src/presentation/controllers/http/role/dtos/role.dto.ts
+++ src/presentation/controllers/http/role/dtos/role.dto.ts
@@ -14,11 +14,14 @@ export class RoleDto extends BasicDto {
 
   @ApiPropertyOptional({
     description: 'Unique identifier of the entity',
-    example: '[f47ac10b-58cc-4372-a567-0e02b2c3d475]',
+    example: "['f47ac10b-58cc-4372-a567-0e02b2c3d475']",
     required: false,
   })
   permissionIds?: string[]
 
-  @ApiPropertyOptional({ type: CreatePermissionDto })
+  @ApiPropertyOptional({
+    type: CreatePermissionDto,
+    required: false,
+  })
   permissions?: CreatePermissionDto[]
 }
