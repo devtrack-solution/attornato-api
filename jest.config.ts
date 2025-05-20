87482fd79961422048f5ebb6f6dcbf383e71a22a
Laercio <laercio.aaavila@gmail.com>
2025-05-20 10:14:24 -0300

    improvement tips codex for test

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest'
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
     '^.+\\.(t|j)s$': ['ts-jest', {
