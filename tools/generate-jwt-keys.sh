2a98f12925a561e2e6254d236b6096f242c9e0e8
Laercio <laercio.aaavila@gmail.com>
2025-06-11 11:57:48 -0300

    refactor: fix

--- tools/generate-jwt-keys.sh
+++ tools/generate-jwt-keys.sh
@@ -2,11 +2,16 @@
 
 export PROJECT=attornato
 
-openssl genrsa -out ./tools/${PROJECT}-private.pem 2048
+# Cria o diretório caso não exista
+mkdir -p ./tools/pems
 
-openssl rsa -in ./tools/${PROJECT}-private.pem -pubout > ./tools/${PROJECT}-public.pem
+# Gera chave privada
+openssl genrsa -out ./tools/pems/${PROJECT}-private.pem 2048
 
+# Gera chave pública a partir da chave privada
+openssl rsa -in ./tools/pems/${PROJECT}-private.pem -pubout > ./tools/pems/${PROJECT}-public.pem
+
+# Exibe instruções formatadas para adicionar ao .env
 echo -e "\e[32m\e[1mTo setup the JWT keys, please add the following values to your .env file:\e[0m"
-echo "SECURITY_JWT_PUBLIC_KEY_BASE64=\"$(base64 -i ./tools/${PROJECT}-public.pem)\""
-echo "SECURITY_JWT_PRIVATE_KEY_BASE64=\"$(base64 -i ./tools/${PROJECT}-private.pem)\""
-
+echo "SECURITY_JWT_PUBLIC_KEY_BASE64=\"$(base64 -i ./tools/pems/${PROJECT}-public.pem)\""
+echo "SECURITY_JWT_PRIVATE_KEY_BASE64=\"$(base64 -i ./tools/pems/${PROJECT}-private.pem)\""
